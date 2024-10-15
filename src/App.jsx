7a0fe526f5844b37338136de109a6a260d702769
ShivaAili <ailishivaprasad112@gmail.com>
2024-10-15 17:45:01 +0530

    App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Adminprofile from "../src/components/Adminprofile/Home/home"
+import Adminprofile from "../src/components/Adminprofile/Home/home";
 import Jobcontainer from './components/Jobdetailes/Jobcontainer';
 import Homepage from './components/Home/Homepage/Home';
-import "./App.css"
-<<<<<<< HEAD
-// import LoginPagee from './components/LoginPage/LoginPage';
-=======
+import "./App.css";
 
->>>>>>> be8196f40458d3eb1890a5730eeddd88ede6c1b2
+// Uncomment if you want to use LoginPagee
+// import LoginPagee from './components/LoginPage/LoginPage';
 
 export default function App() {
-
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path='/adminProfile' element={<Adminprofile/>}/>
-<<<<<<< HEAD
-      <Route path='/jobDetailes' element={<Jobcontainer/>}/>
-      {/* <Route path='/login' element={<LoginPagee/>}/> */}
-=======
-      <Route path='/jobDetailes' element={<Jobcontainer/>}/> 
->>>>>>> be8196f40458d3eb1890a5730eeddd88ede6c1b2
-     
-      
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/adminProfile" element={<Adminprofile />} />
+        <Route path="/jobDetailes" element={<Jobcontainer />} />
+        {/* Uncomment the following line if LoginPagee is needed */}
+        {/* <Route path="/login" element={<LoginPagee />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
