c7be716614d89f8457602e6f19ca95c986c15abb
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2024-12-09 13:10:14 +0530

    fix job page link

--- src/components/Home/Header/Job/Job.jsx
+++ src/components/Home/Header/Job/Job.jsx
@@ -73,7 +73,7 @@ export default function Job()
                            
               
                           }}
-                    to="/jobDetailes">{job}</Link>
+                    to="/job-details">{job}</Link>
                   
     
               ))
